cbfd634799583aacd85fc1bf0478b5c88aade745
kumaramit124 <kumaramit124@gmail.com>
2017-01-10 08:00:41 +0530

    Create sample.c

---
+++ sample.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+//#include"/opt/poky/1.8/sysroots/armv7a-vfp-neon-poky-linux-gnueabi/usr/include/stdio.h"
+int main()
+{
+
+        printf("Hellow World Test!!!!!!!\n");
+        return 0;
+}
